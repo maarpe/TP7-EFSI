adf37d9a093691acb48445c95497424bcd4e1503
MARTIN ARIEL PEREZ <47026770@ort>
2023-08-03 10:35:49 -0300

    arreglar card y carrousel de la home

--- src/Componentes/Home.js
+++ src/Componentes/Home.js
@@ -1,110 +1,93 @@
 import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
 import axios from 'axios';
 import bootstrap from 'bootstrap/dist/js/bootstrap.js';
 import { useState, useEffect } from 'react';
-import Foto from '../foto.jpg';
-import Foto2 from '../foto2.jpg';
-import Foto3 from '../foto3.jpg';
+import { resolvePath } from 'react-router-dom';
 
 function Home() {
-    const [nombreProducto, setNombreProducto] = useState('');
     const [productos, setProductos] = useState([]);
-    const [randomProductos, setRandomProductos] = useState({});
+    const [randomProducto, setRandomProductos] = useState({});
 
-    /*const traerProductos = (url) => {
-        axios.get(url)
-            .then(res => {
-                const productosUl = document.getElementById("productos");
-                const textoIngresado = document.getElementById("textoIngresado")
-                productosUl.innerHTML = "";
-
-                res.data.products.forEach(element => {
-                    const unLi = document.createElement("li");
-
-                    unLi.innerHTML = `<b> ${element.title} </b> <a href="#" data-toggle="modal" data-target="#exampleModal" onclick="traerInformacion(${element.id})">Ver Detalle </a>`;
-
-                    productosUl.appendChild(unLi); //agrega un valor al final de una lista
-                });
-
-                //document.getElementById("buscar").onclick = () => traerProductos("https://dummyjson.com/products/search?q=" + textoIngresado.value);
-
-                console.log(res);
+    useEffect(() => {
+        axios.get('https://dummyjson.com/products/search?q=')
+            .then((response) => {
+                const productosResponse = response.data.products;
+                setProductos(productosResponse);
             });
+    }, []);
+
+    if (productos.length == 0) return (<></>);
+
+    function ProductoAleatorio() {
+        let rand = Math.floor(Math.random() * productos.length);
+        return productos[rand];
     }
 
-    traerProductos("https://dummyjson.com/products");
-    */
-    useEffect(() => {
-        axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
-          .then((response) => {
-            const productosResponse = response.data.data;
-            setProductos(productosResponse);
-    
-            const randomProductos = productosResponse[Math.floor(Math.random() * productosResponse.length)];
-            setRandomProductos(randomProductos);
-          });
-      }, []);
-    
+    let productosRandom = [ProductoAleatorio(), ProductoAleatorio(), ProductoAleatorio(), ProductoAleatorio(), ProductoAleatorio(), ProductoAleatorio(), ProductoAleatorio(), ProductoAleatorio(), ProductoAleatorio()];
+
     return (
-        <Carousel>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Foto}
-                    alt="New York"
-                    height="500px"
-                />
-                <Carousel.Caption>
-                    <h3>Nombre Producto</h3>
-                    <p>Desc</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Foto2}
-                    alt="Second"
-                    height="500px"
-                />
+        <>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-200"
+                        src={productosRandom[0].images[0]}
+                        height="400px"
+                    />
+                    <Carousel.Caption>
+                        <h3>{productosRandom[0].title}</h3>
+                        <p>{productosRandom[0].description}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-200"
+                        src={productosRandom[1].images[0]}
+                        alt="Second"
+                        height="400px"
+                    />
 
-                <Carousel.Caption>
-                    <h3>Nombre Producto</h3>
-                    <p>Desc</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src={Foto3}
-                    alt="Third"
-                    height="500px"
-                />
+                    <Carousel.Caption>
+                        <h3>{productosRandom[1].title}</h3>
+                        <p>{productosRandom[1].description}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-200"
+                        src={productosRandom[2].images[0]}
+                        alt="Third"
+                        height="400px"
+                    />
 
-                <Carousel.Caption>
-                    <h3>Nombre Producto</h3>
-                    <p>Desc</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+                    <Carousel.Caption>
+                        <h3>{productosRandom[2].title}</h3>
+                        <p>{productosRandom[2].description}</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+
+            </Carousel>
+            <br/>
+            <br/>
+            {productos.slice(0,5).map((producto) => (
+                <Card>
+                    <div className="card row" style={{width: "18rem"}}>
+                        <img src={producto.images} className="card-img-top">
+                        </img>
+                        <div className="card-body">
+                            <h5 className="card-title">{producto.title}</h5>
+                            <p className="card-text">{producto.description}</p>
+                            <a href="#" className="btn btn-primary">Comprar</a>
+                        </div>
+                        
+                    </div>
+                </Card>
+            ))}
+        </>
     );
 
- 
 
-
-    /*const traerInformacion = (id) => {
-        const myModal = new bootstrap.Modal(document.getElementById('exampleModal'));
-        console.log(id)
-        axios.get("https://dummyjson.com/products/" + id)
-            .then(res => {
-                const tituloModal = document.getElementById("tituloProdModal");
-                const textoInfo = document.getElementById("modalInfo");
-                tituloModal.innerHTML = res.data.title;
-                textoInfo.innerHTML = `<b> ${res.data.brand} </b><p> ${res.data.category} </p> <p> ${res.data.description} </p>`;
-                console.log(res);
-                myModal.show();
-            });
-    }
-*/
 }
 
 export default Home;
